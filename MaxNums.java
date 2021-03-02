97937c33e9430efdcc474e8d9db7ff0c0671c662
katiegildersleeve <69777555+katiegildersleeve@users.noreply.github.com>
2021-03-02 13:20:09 -0600

    Update MaxNums.java

--- MaxNums.java
+++ MaxNums.java
@@ -8,6 +8,16 @@ class MaxNums {
      * https://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
      */
    public int maxNums(int[] nums, int n) {
-      return -1;
+     PriorityQueue<Integer> numbers = new PriorityQueue();
+     for(int i = 0; i < nums.length; i++){
+       numbers.add(nums[i]);
+     }
+     int product = 1;
+     for(int i = 0; i < n; i++){
+       int cur = (int)numbers.peek();
+       product *= cur;
+       numbers.remove(cur);
+     }
+      return product;
    }
 }
